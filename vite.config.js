954500d94a8559d4705060d0046631ab1ba01dce
wuyeelok <wuyeelok@gmail.com>
2024-03-14 13:14:09 +0800

    Vite: Fix prod build muti page config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import htmlPurge from "vite-plugin-purgecss";
+import { resolve } from "path";
 
 export default {
   base: "/tm_flixx-app/",
   plugins: [htmlPurge()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        movie_details: resolve(__dirname, "movie-details.html"),
+        search: resolve(__dirname, "search.html"),
+        shows: resolve(__dirname, "shows.html"),
+        tv_details: resolve(__dirname, "tv-details.html"),
+      },
+    },
+  },
 };
