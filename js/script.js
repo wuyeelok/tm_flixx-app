ff5fd90ee9dd75ba6798e0e07966f80d9197a3b8
wuyeelok <wuyeelok@gmail.com>
2024-03-16 11:06:14 +0800

    Style: Higlight active link

--- js/script.js
+++ js/script.js
@@ -2,7 +2,20 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+function highlightActiveLink(text) {
+  const links = document.querySelectorAll(".nav-link");
+
+  for (const link of links) {
+    if (text === link.innerText) {
+      link.classList.add("active");
+      break;
+    }
+  }
+}
+
 function init() {
+  let targetLinkText = "";
+
   switch (true) {
     case global.currentPage.includes("movie-details.html"):
       console.log("Movie Details");
@@ -11,14 +24,18 @@ function init() {
       console.log("Search");
       break;
     case global.currentPage.includes("shows.html"):
+      targetLinkText = "TV Shows";
       console.log("Shows");
       break;
     case global.currentPage.includes("tv-details.html"):
       console.log("TV Details");
       break;
     default:
+      targetLinkText = "Movies";
       console.log("Home");
   }
+
+  highlightActiveLink(targetLinkText);
 }
 
 document.addEventListener("DOMContentLoaded", init);
