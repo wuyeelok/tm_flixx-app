78aa8b85b42527a520bd64764750561284cf37af
wuyeelok <wuyeelok@gmail.com>
2024-03-16 19:21:16 +0800

    Fetch: Able to get and show 20 most popular movies

--- js/script.js
+++ js/script.js
@@ -1,6 +1,73 @@
 const global = {
-  currentPage: window.location.pathname,
+  currentPage: getCurrentPageHTMLFilePath(),
+  API_KEY: "7afb6867335b56cd49dfc5b14c686f20",
+  API_URL: "https://api.themoviedb.org/3/",
 };
+
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+
+  const imageEndPoint = "https://image.tmdb.org/t/p/w500/";
+
+  for (let i = 0; i < results.length; i++) {
+    const movie = results[i];
+
+    const releaseDate = new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    }).format(new Date(movie.release_date));
+
+    const cardDivEle = document.createElement("div");
+    cardDivEle.classList.add("card");
+
+    let imageSrc = "images/no-image.jpg";
+
+    if (movie.poster_path) {
+      imageSrc = `${imageEndPoint}${movie.poster_path}`;
+    }
+
+    cardDivEle.innerHTML = `
+    <a href="movie-details.html?id=1">
+      <img
+        src="${imageSrc}"
+        class="card-img-top"
+        alt="${movie.title}"
+      />
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${movie.title}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${releaseDate}</small>
+      </p>
+    </div>
+  `;
+
+    document.getElementById("popular-movies").appendChild(cardDivEle);
+  }
+}
+
+// Get data from TMDB API
+async function fetchAPIData(endpoint) {
+  const spinner = document.querySelector(".spinner");
+  spinner.classList.add("show");
+
+  const response = await fetch(
+    `${global.API_URL}${endpoint}?api_key=${global.API_KEY}&language=en-US`
+  );
+
+  try {
+    if (!response.ok || response.status !== 200) {
+      throw new Error("Something is wrong");
+    }
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    spinner.classList.remove("show");
+  }
+}
 
 function getCurrentPageHTMLFilePath() {
   const fullPathname = window.location.pathname;
@@ -49,6 +116,7 @@ function init() {
     case "/":
     case "index.html":
       console.log("home");
+      displayPopularMovies();
       break;
     case "movie-details.html":
       console.log("Movie Details");
